5174b46431ca8527623da2dc534234e8a711d82d
ThomasBeWeb <tcantinelli@gmail.com>
2018-05-14 17:00:01 +0200

    Affichage cartes

--- contents/cartesOld.php
+++ contents/cartesOld.php
@@ -4,46 +4,64 @@
 <hr>
 <!-- Import de la carte -->
 <?php
-if ($stream = fopen('https://whispering-anchorage-52809.herokuapp.com/cartes/1/get', 'r')) {
-            
-        $lastCarteJson = stream_get_contents($stream, -1, 0);
+//Recuperation de la liste des ID des cartes Online
 
-        fclose($stream);
+if ($stream = fopen('https://whispering-anchorage-52809.herokuapp.com/cartes/getonline', 'r')) {
+            
+        $listeIDsJson = stream_get_contents($stream, -1, 0);
 
         //Conversion en tableau
-        $lastCarte = json_decode($lastCarteJson, true);
+        $listeIDs = json_decode($listeIDsJson, true);
+
+        fclose($stream);
 }
-?>
 
-<div class="row justify-content-center">
-        <h2 id=<?=$lastCarte['nom>'];?></h2>
-</div>
-<div class="d-flex flex-column">
-</div>
+//Pour chaque carte, recup des infos et affichage
 
-<?php
-foreach($lastCarte['menu'] as $value){
-?>
-<div class="d-flex flex-column petiteCarte">
-        <div class="p-2">
-                <h3>menu enfant</h3>
-                <div class="p-2 d-flex flex-row justify-content-around">
-                        <div class="p-2">
-                                <h4>Entrée:</h4>
-                                <h4>Salade</h4>
-                        </div>
-                        <div class="p-2">
-                                <h4>Plat:</h4>
-                                <h4>Steak frites</h4>
-                        </div>
-                        <div class="p-2">
-                                <h4>Entrée:</h4>
-                                <h4>Crême brulée</h4>
+foreach($listeIDs as $idCarte){
+
+        if ($stream = fopen('https://whispering-anchorage-52809.herokuapp.com/cartes/'.$idCarte.'/get', 'r')) {
+            
+                $myCardJson = stream_get_contents($stream, -1, 0);
+        
+                fclose($stream);
+        
+                //Conversion en tableau
+                $myCard = json_decode($myCardJson, true);
+        }
+        ?>
+        
+        <div class="row justify-content-center">
+                <h2><?=$myCard['nom>'];?></h2>
+        </div>
+        <div class="d-flex flex-column">
+        </div>
+        
+        <?php
+        foreach($myCard['menu'] as $value){
+        ?>
+        <div class="d-flex flex-column petiteCarte">
+                <div class="p-2">
+                        <h3><?= $value['nom']; ?></h3>
+                        <div class="p-2 d-flex flex-row justify-content-around">
+                                <div class="p-2">
+                                        <h4>Entrée:</h4>
+                                        <h4><?= $value['entree']['nom']; ?></h4>
+                                </div>
+                                <div class="p-2">
+                                        <h4>Plat:</h4>
+                                        <h4><?= $value['plat']['nom']; ?></h4>
+                                </div>
+                                <div class="p-2">
+                                        <h4>Dessert:</h4>
+                                        <h4><?= $value['dessert']['nom']; ?></h4>
+                                </div>
                         </div>
                 </div>
         </div>
-</div>
+        <?php
+        }
 
-<?php
 }
+
 ?>
